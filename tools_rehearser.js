c1af96f768b6ae50d08a13b535b6099036c75c6e
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-05-01 19:44:08 +0200

    Question checking mechanism

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -1,14 +1,28 @@
+let question_dict = {
+    "Question 1": "Test",
+    "Question 2": "Test",
+    "Question 3": "Test",
+    "Question 4": "Test",
+    "Question 5": "Test",
+};
+
 function checkAnswer(){
+
+    const title_text = document.getElementById('question-title')
+
     const textfield = document.getElementById('text-field');
     const given_answer = textfield.value;
 
-    console.log(given_answer);
+    console.log("The given answer is: " + given_answer);
 
-    textfield.value = "";
+    if (given_answer = question_dict[title_text]) {
+        console.log("The answer is correct");
+        textfield.value = "";
+    } else {
+        console.log("The answer was incorrect");
+    };
 
-}
-
-
+};
 
 function setProgress(){
     const ogham = document.getElementById('saille')
@@ -22,4 +36,4 @@ function setProgress(){
     if (ogham_class = "ogham-zero") {
         ogham.style.height = "6vh";
     };
-}
+};
