b583ff29b19e2c40921ca8de7a0f4cc9312c45e1
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-05-01 16:21:00 +0200

    Testing the ogham bar

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -1,8 +1,7 @@
 function createNew(){
-    const div = document.createElement("div");
+    const ogham = document.getElementById('beith')
 
-    div.id = "active";
-    div.className = "card";
-    
-    document.body.appendChild(div);
+    if (ogham.getAttribute("class") = "ogham-zero") {
+        ogham.setAttribute("class", "ogham-one")
+    }
 }
