18b4515b50ea1f3ab9d821b9ce1062895534bef0
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-05-02 19:08:44 +0200

    Possible typo?

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -29,7 +29,7 @@ let questions = [];
 
 let databases = {
     "Skeletal System - Bones": {"Database": './data_anatomy_skeletal_bones.json', "Questions": ["Division", "Parts"]},
-    "Circulatory System - Arteries": {"Database": './data_anatomy_circulatory_arterial.json', "Questions": ["Branches", "Parts"]}
+    "Circulatory System - Arteries": {"Database": './data_anatomy_circulatory_arteries.json', "Questions": ["Branches", "Parts"]}
 };
 
 let locations = {
