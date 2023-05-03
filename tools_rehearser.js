32558431e0d5f92b38e5f9996b6568a3168c4899
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-05-03 12:34:35 +0200

    Check

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -225,7 +225,7 @@ function chooseQuestions(questions){
         for (var key in side_questions){
 
             console.log("--> Going through " + key);
-            console.log(side_questions.key)
+            console.log(side_questions[key])
 
             let current_side_question = side_questions[side_keys[j]];
 
