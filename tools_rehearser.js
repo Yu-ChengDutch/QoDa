1f187b08906a7beb3e35c41e4b41e10ec76a93a3
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-05-09 10:21:58 +0200

    Update tools_rehearser.js

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -20,7 +20,7 @@ let question_texts = {
     "Gender": ["Certain structures only occur in males or females. Whom does <", "> occur in?"],
     "Subclass": ["Medication always belongs to a class. To what class does <", "> belong?"],
     "Brands": ["Medication is produced by different brands. Of what medication is the brand <", "> an instance?"],
-    "Method": ["Medication may be taken in ways such as oral, intramuscular injection, IUD etc. With what method is <", "> taken?"
+    "Method": ["Medication may be taken in ways such as oral, intramuscular injection, IUD etc. With what method is <", "> taken?"]
 };
 
 let possible_questions = {};
