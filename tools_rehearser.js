07502b5cb16f7438c5380ed5cc723a46bcc6c7a2
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-07-25 18:26:53 +0200

    Added ENT

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -42,6 +42,7 @@ let current_database = ""
 let questions = [];
 
 let databases = {
+    "Pathology - Ophthalmology": {"Database": './data_pathology_ophtha.json', "Iterators": ["Division"], "Questions": ["Definition", "Signs", "Subconditions", "Epidemiology", "Alternative name", "Complications"]},
     "Pathology - Ophthalmology": {"Database": './data_pathology_ophtha.json', "Iterators": ["Individual conditions", "Subtypes"], "Questions": ["Definition", "Signs", "Subconditions", "Epidemiology", "Alternative name"]},
     "Pathology - Dermatology and Venereology": {"Database": './data_pathology_derm.json', "Iterators": ["Individual conditions", "Subtypes"], "Questions": ["Pathogen", "Signs", "Subconditions", "Epidemiology", "Alternative name", "Dutch name"]},
     "Pharmacology - N": {"Database": './data_pharmacology_n.json', "Questions": ["Subclass", "Brands"]},
