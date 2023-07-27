f0110611b6671320218dfc48d3b6b1f1e957fb66
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-07-27 20:53:42 +0200

    New data from test

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -44,7 +44,7 @@ let questions = [];
 
 let databases = {
     "Pathology - ENT": {"Database": './data_pathology_ENT.json', "Iterators": ["Division"], "Questions": ["Definition", "Signs", "Subconditions", "Epidemiology", "Alternative name", "Complications"]},
-    "Pathology - Ophthalmology": {"Database": './data_pathology_ophtha.json', "Iterators": ["Individual conditions", "Subtypes"], "Questions": ["Definition", "Signs", "Subconditions", "Epidemiology", "Alternative name"]},
+    "Pathology - Ophthalmology": {"Database": './data_pathology_ophtha.json', "Iterators": ["Individual conditions", "Subtypes"], "Questions": ["Definition", "Signs", "Subconditions"]}, //"Epidemiology", "Alternative name"
     "Pathology - Dermatology and Venereology": {"Database": './data_pathology_derm.json', "Iterators": ["Individual conditions", "Subtypes"], "Questions": ["Pathogen", "Signs", "Subconditions", "Epidemiology", "Alternative name", "Dutch name"]},
     "Pharmacology - N": {"Database": './data_pharmacology_n.json', "Questions": ["Subclass", "Brands"]},
     "Pharmacology - G": {"Database": './data_pharmacology_g.json', "Questions": ["Subclass", "Brands", "Alternative name", "Method"]},
