09132e56ee307c7ae6468b333f2bf04f476c22e3
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-09-12 16:18:19 +0200

    Addition

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -152,6 +152,7 @@ function setUp(){
 function setMnemonicQuestion(database){
 
     console.log("- > Setting mnemonic question")
+    console.log(database)
 
     nr_q = database.length
     starting_point = Math.floor(Math.random() * nr_q);
@@ -159,7 +160,7 @@ function setMnemonicQuestion(database){
     console.log("- > There are a total of " + nr_q + " questions.");
     console.log("- > We're starting at " + starting_point)
 
-    document.getElementById('question-title').innerText = "Question " + starting_point
+    document.getElementById('question-title').innerText = "Question " + (starting_point + 1)
     document.getElementById('question-description').innerText = database[5].Question
 
 };
